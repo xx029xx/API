7ae36ace19e329d8c23008fc0b73bf8ce7644a4b
Natalia Zvereva <xx029xx@ya.ru>
2023-01-28 17:41:12 +0300

    initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String name;
+    public String patronymic;
+    public String surname;
+    public FormDate birthday;
+    public String passport;
+    public String phone;
+    public boolean subscription;
+}
